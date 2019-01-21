5a9282bd8246af6e100dc092e8965341c50c3652
Mike Landers <landers.mike@gmail.com>
2019-01-21 11:33:12 -0800

    save movie works

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
 	constructor(props) {
@@ -11,11 +11,13 @@ export default class SavedList extends Component {
 			<div className="saved-list">
 				<h3>Saved Movies:</h3>
 				{this.props.list.map(movie => (
-					<span className="saved-movie">{movie.title}</span>
+					<Link to={`/movies/${movie.id}`} className="saved-movie">
+						{movie.title}
+					</Link>
 				))}
-				<Link to="/" className="home-button">
+				<NavLink to="/" className="home-button">
 					Home
-				</Link>
+				</NavLink>
 			</div>
 		);
 	}
