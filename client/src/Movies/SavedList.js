044eafe345edd64d4723c99862191733d71e4380
Mike Landers <landers.mike@gmail.com>
2019-01-22 12:32:08 -0800

    MVP & Stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,9 +11,9 @@ export default class SavedList extends Component {
 			<div className="saved-list">
 				<h3>Saved Movies:</h3>
 				{this.props.list.map(movie => (
-					<Link to={`/movies/${movie.id}`} className="saved-movie">
+					<NavLink to={`/movies/${movie.id}`} className="saved-movie">
 						{movie.title}
-					</Link>
+					</NavLink>
 				))}
 				<NavLink to="/" className="home-button">
 					Home
